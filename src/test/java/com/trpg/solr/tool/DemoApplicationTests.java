4741639d9dd7eaa51e855781f8fcf6fa03d2ac26
SomaPrime <30965261+SomaPrime@users.noreply.github.com>
2017-09-03 21:08:09 +0900

    firstcommit

---
+++ src/test/java/com/trpg/solr/tool/DemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.trpg.solr.tool;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
